2e00c6104822ba1fed64397d707e76bb870b3470
Marta Janicka <mjanicka@sigma.inf.ug.edu.pl>
2012-12-04 14:50:00 +0100

    dodanie walidacji

--- patient_card/app/models/patient.rb
+++ patient_card/app/models/patient.rb
@@ -1,3 +1,11 @@
 class Patient < ActiveRecord::Base
   attr_accessible :city, :date_of_birth, :email, :medical_history, :name, :number, :pesel, :phone, :street, :surname, :zip_code
+  validates :pesel, length: {
+	minimum: 11,
+	maximum: 11
+  }
+  validates :pesel, uniqueness: true
+  validates :phone, :pesel, numericality: { only_integer: true }
+  validates :email, uniqueness: true, format: { :with => /^[a-zA-Z0-9.\-_]+\@[a-zA-Z0-9.\-_]+\.[a-z]{2,4}$/ }
+  validates :zip_code, format: { :with => /^[0-9]{2}+\-[0-9]{3}$/ }  
 end
